b1836cc1cfceae9a2b64308a6d8e134b3518928b
Wei <a951528@gmail.com>
2021-07-01 12:57:03 +0800

    範例14-7 & 14-8

--- src/main/kotlin/Room.kt
+++ src/main/kotlin/Room.kt
@@ -1,10 +1,14 @@
 
 open class Room(val name:String) {
-    fun description() = "Room: $name"
+    protected open val dangerlevel = 5
+    fun description() = "當前所在地: $name\n" + "危險等級: $dangerlevel"
 
     open fun load() = "沒什麼特別的"
 }
 
 class TownSquare : Room("Town Square"){
+    override val dangerlevel = super.dangerlevel - 3
+
     override fun load() = "你來到了小鎮的廣場!"
+
 }
